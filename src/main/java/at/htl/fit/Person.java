e3100779eec38ac6d0f96d40ac34cfede0c52a72
bojernico <nico.bojer@gmail.com>
2021-10-21 13:55:40 +0200

    initial commit

---
+++ src/main/java/at/htl/fit/Person.java
@@ -0,0 +1,75 @@
+package at.htl.fit;
+
+import javax.json.bind.annotation.JsonbProperty;
+import javax.json.bind.annotation.JsonbTransient;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.Instant;
+
+@Entity
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @JsonbProperty("first_name")
+    private String firstName;
+
+    @JsonbProperty("last_name")
+    private String lastName;
+
+    private String email;
+
+    @JsonbProperty("telephone_no")
+    private String telephoneNo;
+
+    @JsonbTransient
+    private Instant timestamp;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setTelephoneNo(String telephoneNo) {
+        this.telephoneNo = telephoneNo;
+    }
+
+    public String getTelephoneNo() {
+        return telephoneNo;
+    }
+
+    public Instant getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Instant timestamp) {
+        this.timestamp = timestamp;
+    }
+}
